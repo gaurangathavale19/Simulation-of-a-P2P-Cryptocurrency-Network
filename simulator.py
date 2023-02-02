434330d0a3425d80a6ea06ad2ebcfcdfa80226b2
virusundetected <gaurangathavale@gmail.com>
2023-02-02 23:54:07 +0530

    Completed 4 and 5

--- simulator.py
+++ simulator.py
@@ -1,8 +1,13 @@
 import argparse
 import random
+import numpy as np
 from node import Node
+# nodes = []
 
+
+# def sim():
 if __name__ == "__main__":
+    # global nodes
 
     # Command line arguments
     parser = argparse.ArgumentParser()
@@ -25,9 +30,9 @@ if __name__ == "__main__":
     print('Number of slow nodes:', number_of_slow_nodes)
     print('Number of low CPU nodes:', number_of_low_CPU_nodes)
 
-    nodes = []
     speeds = []
     computation_powers = []
+    nodes = []
 
     for i in range(n):
         if(number_of_slow_nodes > 0):
@@ -56,8 +61,11 @@ if __name__ == "__main__":
 
     adj_matrix = [[0 for i in range(n)] for j in range(n)]
 
-    min1 = 4
-    max1 = min(n-1, 8)
+    for i in range(n-1):
+        adj_matrix[i][i+1] = 1
+
+    min1 = 3
+    max1 = min(n-2, 7)
 
     for i in range(n):
         for j in range(i):
@@ -67,17 +75,44 @@ if __name__ == "__main__":
         test = [1]*number_of_ones + [0]*(n-i-1-number_of_ones)
         random.shuffle(test)
         
-        j=i+1
+        j=i+2
         k=0
         while(j<n):
             adj_matrix[i][j] = test[k]
             k += 1
             j += 1
-    
+
     print(adj_matrix)
-
-    
-
 
     ##### End 4 #####
 
+    ##### Start 5 #####
+    def send_message(message, sender_id, receiver_id):
+        if(nodes[sender_id].speed == 1 and nodes[receiver_id].speed == 1):
+            c = 100 * 10**6
+        else:
+            c = 5 * 10**6
+       
+        m = len(message)
+
+        # d from exponential dist with mean 96/c
+        
+        mean = 96 * 10**3 / c
+        d = np.random.exponential(mean, 1)[0]
+
+        p = np.random.uniform(low=10, high=500)
+
+        # latency
+        latency=p+m/c+d
+
+        print('Latency from ' + str(sender_id) + ' to ' + str(receiver_id) + ':', latency)
+    
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if(adj_matrix[i][j] == 1):
+                message = 'TxnID: ' + str(i) + ' pays ' + str(j) + ' 10 coins'
+                send_message(message, i, j)
+        
+    ##### End 5 #####
+
+    
--- transaction.py
+++ transaction.py
@@ -1,8 +1,15 @@
+# from simulator import *
+
 class Transaction:
     def __init__(self, transaction_id, sender_id, receiver_id, coins):
         self.transaction_id = transaction_id
         self.sender_id = sender_id
         self.receiver_id = receiver_id
         self.coins = coins
+
+    # sim()
+
+    # def check_balance_of_sender(message, nodes):
+
 
     
