6c2d89a167f0beed665ffab322817b7f21200d84
virusundetected <gaurangathavale@gmail.com>
2023-02-15 14:42:19 +0530

    added function comments

--- block.py
+++ block.py
@@ -1,6 +1,13 @@
 import hashlib
 class Block:
     def __init__(self, creator_id, creation_time, peer_balance, transaction_list, previous_block_hash):
+        '''
+            -creator_id: ID of the miner
+            -creation_time: Creation time of the block
+            -peer_balance: Peer balance of all the nodes after processing of the block
+            -transaction_list: List of all transactions in the block (including coinbase transaction)
+            -previous_block_hash: Block hash of the previous block i.e. parent block hash
+        '''
         self.creator_id = creator_id
         self.creation_time = creation_time
         self.transaction_list = transaction_list
@@ -9,6 +16,7 @@ class Block:
         self.block_id = self.set_id()
         self.peers_visited = []
 
+    # Create the block id by hashing the conccatenation of transaction message and the previous block hash
     def set_id(self):
         result = " ".join([txn.transaction_message for txn in self.transaction_list]) + " " + str(self.previous_block_hash)
         return hashlib.sha256(result.encode('utf-8')).hexdigest()
--- event.py
+++ event.py
@@ -1,6 +1,14 @@
 class Event:
 
     def __init__(self, curr_node, type, event_data, sender_id, receiver_id, event_start_time):
+        '''
+            -curr_node: Current location of the node, where the event is present
+            -type: Type of event i.e. BLK or TXN
+            -event_data: object of the event
+            -sender_id: ID of the sender node
+            -receiver_id: ID of the receiver node
+            -event_start_time: Event creation time
+        '''
         self.sender_id = sender_id
         self.receiver_id = receiver_id
         self.curr_node = curr_node
@@ -8,6 +16,7 @@ class Event:
         self.event_data = event_data
         self.type = type
 
+    # For min-heap comparisons
     def __lt__(self, event):
         return self.event_start_time < event.event_start_time
 
